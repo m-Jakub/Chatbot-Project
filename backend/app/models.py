ba0769ea09b0ea78e16fb27da619e1e294135f30
bkubaj <bartosz.kubaj.stud@pw.edu.pl>
2024-10-05 16:59:28 +0200

    Updating with main branch data

--- backend/app/models.py
+++ backend/app/models.py
@@ -12,3 +12,13 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+    
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_message = db.Column(db.String(200), nullable=False)
+    bot_response = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return f"Message('{self.user_message}', '{self.bot_response}')"
+    
+
